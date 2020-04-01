aea11316a22df5b7c5ca04ad76144b275488cf37
Kamel MEGHNINE <kamel.meghnine@gmail.com>
2020-04-01 15:21:17 +0200

    Version 2

--- AppShinyVisualisation/server.R
+++ AppShinyVisualisation/server.R
@@ -6,7 +6,7 @@ library(magrittr)
 library(shinydashboard)
 library(readr)
 library(rgrs)
-
+library(DT)
 
 data_weather <- read_csv("Données_météo.csv")
 data_weather$country <- as.factor(data_weather$country)
@@ -15,13 +15,17 @@ data_weather$continent <- as.factor(data_weather$continent)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output, session) {
-    #La partie de Nuage de Points*****************
+    
+    #La partie de Affichage des Données Météo*****************
+    output$dataweather <- DT::renderDataTable(data_weather)
+    
+    #La partie de Nuage de Points*****************************
     output$graph_continent <- renderPlot({
         
         data_weather %>%
             filter(temp < input$bins) %>%
             ggplot(aes(x=temp, y=continent)) + 
-            geom_point() +
+            geom_point(aes(color=continent)) +
             ggtitle("Température par Continent") +
             xlab("Température °C") +
             ylab("Continent")
@@ -32,26 +36,26 @@ shinyServer(function(input, output, session) {
         data_weather %>%
             filter(temp < input$bins) %>%
             ggplot(aes(x=temp, y=country)) + 
-            geom_point() + ggtitle(input$title) +
+            geom_point(aes(color=continent)) +
             ggtitle("Température par Pays") +
             xlab("Température °C") +
             ylab("Pays")
         
     })
     
-    #La partie Histogramme***********************
+    #La partie Histogramme***********************************
     #Histogramme Par Continent 
     by_continent <- group_by(data_weather, continent)
     dt_continent<-summarise(by_continent, temp = mean(temp))
-    hist_continent <- ggplot(dt_continent, aes(continent, temp)) + geom_col(fill="blue") +
+    hist_continent <- ggplot(dt_continent, aes(continent, temp)) + geom_col(aes(fill=continent)) +
             ggtitle("Température moyenne par Continent") +
             xlab("Continent") +
             ylab("Température °C")
     
     #Histogramme Par Pays 
-    by_country <- group_by(data_weather, country)
+    by_country <- group_by(data_weather, country, continent)
     dt_country <- summarise(by_country, temp = mean(temp))
-    hist_country <- ggplot(dt_country, aes(country, temp)) + geom_col(fill="blue") +
+    hist_country <- ggplot(dt_country, aes(country, temp)) + geom_col(aes(fill=continent)) +
         ggtitle("Température moyenne par Pays") +
         xlab("Pays") +
         ylab("Température °C")
@@ -64,7 +68,9 @@ shinyServer(function(input, output, session) {
         }else{
             hist_country
         }
-        
         })
     
+    #La partie de Affichage des Données Météo*****************
+    
+    output$description <- renderText(input$text)
 })
--- AppShinyVisualisation/ui.R
+++ AppShinyVisualisation/ui.R
@@ -14,13 +14,21 @@ dashboardPage(skin = "green",
     dashboardHeader(title = "Dashbord"),
     dashboardSidebar(
         sidebarMenu(
+            menuItem("Données Météo", tabName = "data", icon = icon(name="fa-align-justify",class = "far fa-align-justify", lib = "font-awesome")),
             menuItem("Nuage de Point", tabName = "nuagepoint", icon = icon(name="fa-chart-line",class = "far fa-chart-line", lib = "font-awesome")),
             menuItem("Histogramme", tabName = "histogramme", icon = icon(name="fa-chart-bar",class = "fal fa-chart-bar", lib = "font-awesome"))
         )
     ),
     dashboardBody(
         tabItems(
-            # First tab content
+            #Tab Données Météo
+            tabItem(tabName = "data",
+                fluidRow(
+                    dataTableOutput("dataweather")
+                )
+            ),
+            
+            # Tab Nuage de Point
             tabItem(tabName = "nuagepoint",
                 # Boxes need to be put in a row (or column)
                 fluidRow(
@@ -32,18 +40,22 @@ dashboardPage(skin = "green",
                     box(
                         title = "Paramètres", status = "info", solidHeader = TRUE, width =4,
                         #"Box content here", br(), "More box content",
-                        sliderInput("bins", "Température :", 1, 50, 25),
-                        textInput("text", "Zone de text:")
+                        sliderInput("bins", "Température :", 1, 50, value = c(15, 30)),
+                        textInput("text", "Saisissez votre description :")
                     ),
                     box(
                         title = "Nuage de Point", status = "primary", solidHeader = TRUE,width =8,
                         collapsible = TRUE,
                         plotOutput("graph_country", height = 250)
                     ),
+                    box(
+                        title = "Description", status = "info", solidHeader = TRUE, width =4,
+                        textOutput("description")
+                    ),
                 )
             ),
             
-            # Second tab content
+            # Tab Histogramme
             tabItem(tabName = "histogramme",
                     
                 fluidRow(
